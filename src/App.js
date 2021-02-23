31a5168f684a58ccbd7dac615dd873bfc694c34c
Kadu Lisboa <kadulisboa301@icloud.com>
2021-02-22 22:30:43 -0300

    Add Typing Effect

--- src/App.js
+++ src/App.js
@@ -1,41 +1,26 @@
 import React from 'react';
 import logo from './assets/images/logo.svg';
-import instagram from './assets/svg/instagram.svg';
-import linkedin from './assets/svg/linkedin.svg';
 import './styles/App.css';
 import kadu from './assets/images/kadu.jpg';
-// import { } from 'react-dom';
+import Title from './components/title'
 
 export default function App() {
 
     return (
-        <div class="content">
+        <div className="content">
             <header>
                 <a href="" >
-                    <img class="logo" src={logo} />
+                    <img className="logo" src={logo} />
                 </a>
             </header>
-            <div class="mid">
-                <div class="img">
+            <div className="mid">
+                <div className="img">
                     <img src={kadu} />
                 </div>
-                <div class="title">
-                    <h1>
-                        I'm Kadu<br />Lisboa
-                    </h1>
-                    <p>FullStack Developer</p>
-                    <div class="social" >
-                        <a href="https://www.instagram.com/kadulisboadev/" target="_blank">
-                            <img src={instagram} />
-                        </a>
-                        <a href="https://www.linkedin.com/in/kadulisboa/" target="_blank">
-                            <img src={linkedin} />
-                        </a>
-                    </div>
-                </div>
+                <Title name={["I'm Kadu", <br />, "Lisboa"]} />
 
             </div>
-            <a class="email" href="mailto:contato@kadulisboa.com.br">
+            <a className="email" href="mailto:contato@kadulisboa.com.br">
                 contato@kadulisboa.com.br
             </a>
         </div>
---
+++ src/components/title.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactTypingEffect from 'react-typing-effect';
+import instagram from '../assets/svg/instagram.svg';
+import linkedin from '../assets/svg/linkedin.svg';
+
+export default function Title({ name }) {
+    return (
+        <div className="title">
+            <h1>
+                {name}
+                {/* I'm Kadu<br />Lisboa */}
+            </h1>
+            <p>
+                <ReactTypingEffect speed={100} eraseSpeed={100} typingDelay={500} eraseDelay={800} text={["FullStack Developer", "ReactJS and React Native", "PHP and NodeJS", "MySQL and PostgreSQL"]} />
+            </p>
+            <div className="social" >
+                <a href="https://www.instagram.com/kadulisboadev/" target="_blank">
+                    <img src={instagram} />
+                </a>
+                <a href="https://www.linkedin.com/in/kadulisboa/" target="_blank">
+                    <img src={linkedin} />
+                </a>
+            </div>
+        </div>
+    )
+}
